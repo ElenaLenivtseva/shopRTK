cac4c6632f8ee172108513097cd534de4a6d9ce5
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-06-25 23:13:31 +0700

    В общем dropdown menu работает. Написано оно на примитивном отображении по условию (через usetate либо true, либо false по отображению). Выглядит меню  ужасно, но суть этого проекта не в стилях. Если бы я хотела, чтобы были стили, я бы ни за что не выбрала tailwind css. Я сторонник БЭМ, sass и т.п. Радует, что я уже могу предугадывать действия автора в redux. Т.е. я могу поставить на паузу, сделать самой, а потом сравнить.

--- src/Components/FiltredProducts/FiltredProducts.jsx
+++ src/Components/FiltredProducts/FiltredProducts.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "../ProductCard/ProductCard";
 import "./FiltredProducts.css";
-import '../NavigateButtons/NavigateButtons.css'
+import "../NavigateButtons/NavigateButtons.css";
+
 const FiltredProducts = () => {
   const products = useSelector((state) => state.products.filtredProducts);
   const { type } = useParams();
-  const genderButtons = ['male','female'];
-  const colorButtons = ['red','green', 'purple', 'yellow', 'blue', 'black', 'brown'];
+  const [colorMenuOpen, setColorMenuOpen] = useState(false)
+  const [sizeMenuOpen, setSizeMenuOpen] = useState(false)
+  const genderButtons = ["male", "female"];
+  const colorButtons = [
+    "red",
+    "green",
+    "purple",
+    "yellow",
+    "blue",
+    "black",
+    "brown",
+  ];
+  const sizeButtons = ["S", "M", "L", "XL"];
   return (
     <>
       <div className="pt-16">
@@ -17,27 +29,52 @@ const FiltredProducts = () => {
             {type}
           </h1>
           <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            {genderButtons.map((item, index)=> {
-              return (
-                <div className="" key={index}>
-                  <button className="my_button">{item}</button>
+            <div className="flex items-center">
+              {genderButtons.map((item, index) => {
+                return (
+                  <div className="" key={index}>
+                    <button className="my_button">{item}</button>
+                  </div>
+                );
+              })}
+              <button className="my_button">High price</button>
+
+              <div className="menu">
+                <div className="menuHandler">
+                  <button className='menuOpenButton my_button' onClick={()=>setColorMenuOpen(!colorMenuOpen)}>Select color</button>
                 </div>
-              )
-            })}
-            <button className="my_button">High price</button>
-            <div className="menu">
-              <div className="menuHandler"></div><div className="menuList">
-                {colorButtons.map((item, index)=>{
-                  return (
-                    <div className="menuItem" key={index} style={{color: item}}>
-                      {item}
-                    </div>
-                  )
-                })}
+                {colorMenuOpen && <div className="menuList">
+                  {colorButtons.map((item, index) => {
+                    return (
+                      <div
+                        className="menuItem"
+                        key={index}
+                        style={{ color: item }}
+                      >
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>}
+              </div>
+              <div className="menu">
+                <div className="menuHandler">
+                  <button className='menuOpenButton my_button' onClick={()=>setSizeMenuOpen(!sizeMenuOpen)}>Select color</button>
+                </div>
+                {sizeMenuOpen &&<div className="menuList">
+                  {sizeButtons.map((item, index) => {
+                    return (
+                      <div className="menuItem" key={index}>
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>}
               </div>
             </div>
-          </div>
+            <div className="pr-14">
+              <button>Clear filter</button>
+            </div>
           </div>
         </div>
         <div className="grid-products">
